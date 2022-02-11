386e80a5c61d0c74d9c1174c770e2ed1ed95fcfe
Mariat28 <mariatndagire28@gmail.com>
2022-02-11 17:45:09 +0300

    content section, responsiveness fix, data table

---
+++ components/Table.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="flex flex-col shadow-md rounded-lg border border-slate-300 ">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 bg-red-500">
+      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8 bg-green-500">
+        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg bg-green-500">
+          <table class="min-w-full divide-y divide-gray-200 ">
+            <thead class="bg-white">
+              <tr class="">
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Company</th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">License use</th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Users</th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">About</th>
+                <th scope="col" class="relative px-6 py-3">
+                  <span class="sr-only">Edit</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200">
+              <tr v-for="company in companies" :key="company.email" class="odd:bg-white even:bg-gray-50">
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="flex items-center">
+                    <div class="flex-shrink-0 h-10 w-10">
+                      <img class="h-10 w-10 rounded-full" :src="company.image" alt="" />
+                    </div>
+                    <div class="ml-4">
+                      <div class="text-sm font-medium text-gray-900">
+                        {{ company.company }}
+                      </div>
+                      <div class="text-sm text-gray-500">
+                        {{ company.license }}
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {{ company.license }}
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800" :class="['']"> {{company.status}} </span>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {{ company.role }}
+                </td>
+
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-sm text-gray-900">{{ company.title }}</div>
+                  <div class="text-sm text-gray-500">{{ company.description }}</div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium flex flex-wrap">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                  </svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                  </svg>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const companies = [
+  {
+    company: 'Jane Cooper',
+    title: 'Regional Paradigm Technician',
+    department: 'Optimization',
+    status:'customer',
+    license: 'jane.cooper@example.com',
+    image:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    title: 'Content Curating App',
+    description: 'Brings all your news into one place'
+  },
+    {
+    company: 'Jane Cooper',
+    title: 'Regional Paradigm Technician',
+    department: 'Optimization',
+    role: 'Admin',
+    status:'churned',
+    license: 'jane.cooper@example.com',
+    image:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+  },
+    {
+    company: 'Jane Cooper',
+    title: 'Regional Paradigm Technician',
+    department: 'Optimization',
+    status:'customer',
+    license: 'jane.cooper@example.com',
+    image:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+  },
+  // More companies...
+]
+
+export default {
+  company: 'Table',
+  data() {
+    return {
+      companies,
+    }
+  },
+}
+</script>
---
+++ components/Table2.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="rounded border border-slate-300">
+    <table >
+      <thead class="flex justify-around">
+        <tr>
+          <th>Company</th>
+          <th>License</th>
+          <th>Status</th>
+          <th>Users</th>
+          <th>About</th>
+
+        </tr>
+      </thead>
+    </table>
+  </div>
+</template>
