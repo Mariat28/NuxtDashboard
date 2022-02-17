10b9b81d57c864d416df35e54d0ce4987a88250f
Mariat28 <mariatndagire28@gmail.com>
2022-02-17 12:15:20 +0300

    (fix) static image routes after deployment

--- components/Dashboard.vue
+++ components/Dashboard.vue
@@ -17,11 +17,11 @@
             <NuxtLink to="/" class="pl-3" >User reports</NuxtLink>
           </div>
           <!-- right search  -->
-          <div class="input-group relative w-72 ml-10 mr-2 flex  items-center rounded border border-solid border-gray-300 focus-within:border-blue-600 focus-within:outline-none ">
+          <div class="input-group relative w-72 ml-10 mr-2 flex  items-center rounded-md border border-solid border-gray-300 focus-within:border-blue-600 focus-within:outline-none ">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="#C1C1C1">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
             </svg>
-            <input type="search" class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-neutral-50 bg-clip-padding  rounded-sm transition ease-out m-0 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search">
+            <input type="search" class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding  rounded-sm transition ease-out m-0 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search">
           </div>
         </div>
 
@@ -37,12 +37,12 @@
             <span class="text-gray-400 text-xs md:text-sm font-normal">Track, manage and forecast your customers and orders.</span>
           </div>
           <div class="flex mt-6 lg:mt-0">
-            <button class="rounded bg-white border border-gray-400 pl-3 pr-3 p-1 text-sm flex items-center text-black">
+            <button class="rounded bg-white border border-gray-400 px-2 py-1 text-sm flex items-center text-black">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4  mr-2" fill="none" viewBox="0 0 24 24" stroke="#303030">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
               </svg>
               Import</button>
-              <button class="rounded bg-purple-700 border border-gray-400 pl-2 pr-3 p-1 text-sm flex items-center text-white ml-3 font-light">
+              <button class="rounded bg-purple-700 border border-gray-400 px-2 py-1 text-sm flex items-center text-white ml-3 font-light">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 font-light" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
               </svg>
--- components/Table.vue
+++ components/Table.vue
@@ -34,7 +34,7 @@
                   </div>
                   <div class="flex items-center">
                     <div class="flex-shrink-0 h-10 w-10">
-                      <img class="h-10 w-10 rounded-full" :src="company.image" alt="" />
+                      <img class="h-10 w-10 rounded-full" :src="company.image"  alt="" />
                     </div>
                     <div class="ml-4">
                       <div class="text-sm font-medium text-gray-900">
