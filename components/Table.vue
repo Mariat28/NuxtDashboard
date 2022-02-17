9948f0669276a3223ee61994e5249257f3e98590
Mariat Ndagire <mariatndagire28@gmail.com>
2022-02-17 20:40:01 +0300

    merging changes

--- components/Table.vue
+++ components/Table.vue
@@ -8,7 +8,7 @@
               <tr class="">
                 <th scope="col" class="px-6 py-3 text-left text-sm font-medium text-slate-700  tracking-wider">
                   <div class="form-check">
-                    <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckIndeterminate">
+                    <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckIndeterminate">
                     <label class="form-check-label inline-block text-gray-800" for="flexCheckIndeterminate">
                       Company
                     </label>
@@ -26,7 +26,7 @@
             <tbody class="bg-white divide-y divide-gray-200">
               <tr v-for="company in companies" :key="company.email" class="odd:bg-white even:bg-gray-50">
                 <td class="px-6 py-4 whitespace-nowrap flex items-center">
-                    <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckIndeterminate">
+                    <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckIndeterminate">
                   <div class="flex items-center">
                     <div class="flex-shrink-0 h-10 w-10">
                       <img class="h-10 w-10 rounded-full" :src="company.image" alt="" />
